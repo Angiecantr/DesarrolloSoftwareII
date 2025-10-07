e507dfcb4176c79f4866d9a0112d8777fb261ed9
angiecantr <angiecantor3@gmail.com>
2025-10-06 22:02:52 -0500

    E

--- Final-Proyect/Final-Proyect/src/environments/environments.development.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: true,
-    companyName: 'Starwars',
-    companyNeme2: 'Apps',
-    companySlogan: 'Mundo de Starwars',
-
-};
--- Final-Proyect/Final-Proyect/src/environments/environments.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: false,
-    companyName: 'Starwars',
-    companyNeme2: 'Apps',
-    companySlogan: 'Mundo de Starwars',
-
-};
